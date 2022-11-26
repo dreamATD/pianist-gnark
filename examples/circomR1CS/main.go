365a4961cebb1f219a73a0362850a88137b45379
Tiancheng Xie <tianc.x@berkeley.edu>
2022-11-25 23:10:02 -0800

    change config

--- examples/circomR1CS/main.go
+++ examples/circomR1CS/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"log"
 	_ "net/http/pprof"
 	"os"
+	"runtime/debug"
 	"strconv"
 
 	"github.com/consensys/gnark-crypto/ecc"
@@ -22,32 +24,71 @@ var (
 	vkPath      string
 )
 
+func SendString(str string, target int) {
+	//string to bytes
+	strBytes := []byte(str)
+	//send bytes to slave
+	length := uint64(len(strBytes))
+	lengthBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(lengthBytes, length)
+	mpi.SendBytes(lengthBytes, uint64(target))
+	mpi.SendBytes(strBytes, uint64(target))
+}
+func RecvString() string {
+	lengthBytes, err := mpi.ReceiveBytes(8, 0)
+	if err != nil {
+		panic(err)
+	}
+	length := binary.LittleEndian.Uint64(lengthBytes)
+	strBytes, err := mpi.ReceiveBytes(length, 0)
+	if err != nil {
+		panic(err)
+	}
+	str := string(strBytes)
+	return str
+}
+
 func parseConfig(jsonFile string) {
 	//parse Json file to get the proper circuit path
 	//load json
-	file, err := os.Open(jsonFile)
-	if err != nil {
-		log.Fatal(err)
+	if mpi.SelfRank == 0 {
+		file, err := os.Open(jsonFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		var f interface{}
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		m := f.(map[string]interface{})
+		//get the circuit path
+		circuitPath = m["circuitPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
+		//get the R1CS path
+		r1csPath = m["r1csPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
+		//get the pk path
+		pkPath = m["pkPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
+		//get the vk path
+		vkPath = m["vkPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
+
+		for i := 1; i < int(mpi.WorldSize); i++ {
+			SendString(m["circuitPath."+strconv.Itoa(i)].(string), i)
+			SendString(m["r1csPath."+strconv.Itoa(i)].(string), i)
+			SendString(m["pkPath."+strconv.Itoa(i)].(string), i)
+			SendString(m["vkPath."+strconv.Itoa(i)].(string), i)
+		}
+	} else {
+		circuitPath = RecvString()
+		r1csPath = RecvString()
+		pkPath = RecvString()
+		vkPath = RecvString()
 	}
-	defer file.Close()
-	var f interface{}
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	m := f.(map[string]interface{})
-	//get the circuit path
-	circuitPath = m["circuitPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
-	//get the R1CS path
-	r1csPath = m["r1csPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
-	//get the pk path
-	pkPath = m["pkPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
-	//get the vk path
-	vkPath = m["vkPath."+strconv.Itoa(int(mpi.SelfRank))].(string)
 }
 
 func main() {
+	debug.SetGCPercent(-1)
 	dir, _ := os.Getwd()
 	fmt.Println("working directory: ", dir)
 
