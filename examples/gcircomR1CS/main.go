29026f3c45528268a99fddef4aaef76280a3a1a6
dreamATD <tianyi.liu.08@gmail.com>
2023-09-04 12:41:46 -0500

    Add R1CS test to general circuit

---
+++ examples/gcircomR1CS/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/gpiano"
+	"github.com/consensys/gnark/frontend"
+	"github.com/sunblaze-ucb/simpleMPI/mpi"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	dir, _ := os.Getwd()
+	fmt.Println("working directory: ", dir)
+	ccs, err := ReadR1CS("r1cs")
+	if err != nil {
+		panic(err)
+	}
+	a, b, c := ccs.GetNbVariables()
+	fmt.Println(a, b, c)
+
+	{
+		// Witnesses instantiation. Witness is known only by the prover,
+		// while public w is a public data known by the verifier.
+		var w R1CSCircuit
+		w.Witness = make([]frontend.Variable, 260275)
+		for i := 0; i < len(w.Witness); i++ {
+			w.Witness[i] = frontend.Variable(0)
+		}
+
+		witnessFull, err := frontend.NewWitness(&w, ecc.BN254)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		witnessPublic, err := frontend.NewWitness(&w, ecc.BN254, frontend.PublicOnly())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// public data consists the polynomials describing the constants involved
+		// in the constraints, the polynomial describing the permutation ("grand
+		// product argument"), and the FFT domains.
+		pk, vk, err := gpiano.Setup(ccs, witnessPublic)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		proof, err := gpiano.Prove(ccs, pk, witnessFull)
+		if mpi.SelfRank == 0 {
+			err = gpiano.Verify(proof, vk, witnessPublic)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
