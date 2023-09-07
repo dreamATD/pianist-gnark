cde8f2cd5b6f0b8a0f74023350f3f1b833e48ff2
dreamATD <tianyi.liu.08@gmail.com>
2023-09-06 20:06:11 -0500

    Claim changes

--- examples/piano/main.go
+++ examples/piano/main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Modifications Copyright 2023 Tianyi Liu and Tiancheng Xie
+
 package main
 
 import (
--- test/dkzg_srs.go
+++ test/dkzg_srs.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Modifications Copyright 2023 Tianyi Liu and Tiancheng Xie
+
 package test
 
 import (
